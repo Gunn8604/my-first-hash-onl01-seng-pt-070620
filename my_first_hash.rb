461c20e28e8e2d580f6ee5ee0b50880f8f89c505
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-19 02:47:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,11 +12,11 @@ def retrieval
   the_manifest["oil paintings"]
 end
 
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                }
+# def adding
+#   shipping_manifest = {
+#                   "whale bone corsets" => 5, 
+#                   "porcelain vases" => 2, 
+#                   "oil paintings" => 3
+#                 }
 
-end
+# end
