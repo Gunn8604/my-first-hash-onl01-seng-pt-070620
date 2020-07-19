8f890438995939f9145a2034adb492bc9400bb54
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-19 02:45:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,24 +9,23 @@ def shipping_manifest
 end
 
 def retrieval
-  
-
+  the_manifest["oil paintings"]
 end
 
-# def adding
-#   shipping_manifest = {
-#                   "whale bone corsets" => 5, 
-#                   "porcelain vases" => 2, 
-#                   "oil paintings" => 3
-#                 }
-#   # add 2 muskets to the shipping_manifest hash below
+def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+                }
+  # add 2 muskets to the shipping_manifest hash below
 
 
 
-#   # add 4 gun powder to the shipping_manifest hash below
+  # add 4 gun powder to the shipping_manifest hash below
 
 
 
-#   # return the shipping_manifest hash below
+  # return the shipping_manifest hash below
 
-# end
+end
